942028217294ca66246d294afedc675911fa3252
lixiang198909 <578115175@qq.com>
2016-03-28 12:34:41 +0800

    新增一个类MyVC

---
+++ HelloWorld/HelloWorld/MyVC1.h
@@ -0,0 +1,13 @@
+//
+//  MyVC1.h
+//  HelloWorld
+//
+//  Created by 李翔 on 16/3/28.
+//  Copyright © 2016年 lixiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyVC1 : UIViewController
+
+@end
